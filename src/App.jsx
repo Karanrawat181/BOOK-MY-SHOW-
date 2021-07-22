a54ac42802cbc8272f1d4e810e2dca2ae51e665e
karan rawat <rawatkaran181@gmail.com>
2021-07-22 12:35:44 +0530

    filterPage added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MovieHOC from './HOC/MovieHOC';
 
 import HomePage from './pages/Home';
 import Movie from './pages/Movie';
+import Plays from './pages/Plays.page';
 
 
 import "slick-carousel/slick/slick.css";
@@ -19,6 +20,7 @@ function App() {
 
             <DefaultHOC path="/" exact component={ HomePage } />
             <MovieHOC path="/movie/:id" exact component={ Movie } />
+            <DefaultHOC path="/plays" exact component={Plays} />
         
         </>
     );
